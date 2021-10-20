ecbc08f77fd19ccfbc623f9190bf2da24e1ec978
chen shu ting <a.0066715@gmail.com>
2021-10-20 15:19:12 +0800

    設定 Route 前置

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,12 @@ Route::get('hello/{name?}',['as'=> 'hello.index',function($name='Everybody'){
 return'Hello,'.$name;
 }]);
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('dashboard',function(){
+    return'dashboard';
 });
 
+Route::group(['prefix '=> 'admin'],function(){
+    Route::get('dashboard',function(){
+        return'admin dashboard';
+    });
+});
