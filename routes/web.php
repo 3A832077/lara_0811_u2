77e373664377a3916dae21db38110d819ac8df5c
chen shu ting <a.0066715@gmail.com>
2021-10-20 14:48:31 +0800

    設定 Route 反應

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/',function(){
+    return'welcome';
+});
+
+Route::get('r1',function(){
+    return redirect('r2');
+});
+
+Route::get('r2',function(){
+    return'welcome';
+});
